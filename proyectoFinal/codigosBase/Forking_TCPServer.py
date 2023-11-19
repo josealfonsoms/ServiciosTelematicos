14f77ec2e0af20b0901ca89ad5f5cb5781cc5dc1
Jose Mercado <alfonso.mercado@udea.edu.co>
2023-11-18 23:24:57 -0500

    Agregando carpeta proyectoFinal

---
+++ proyectoFinal/codigosBase/Forking_TCPServer.py
@@ -0,0 +1,13 @@
+from socketserver import ForkingTCPServer, BaseRequestHandler
+
+class myHandler(BaseRequestHandler):
+    def handle(self):
+        print ("Connection from ", str(self.client_address))
+        while True:
+            data = self.request.recv(1024)
+            if data.decode() == "bye\r\n": break
+            self.request.send(data.upper())
+        self.request.close()
+
+myServer = ForkingTCPServer(("127.0.0.1", 3456), myHandler)
+myServer.serve_forever()
